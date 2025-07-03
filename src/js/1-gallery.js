5c125ee472bfd8e0752a91b16fb4addb49f408e3
Andriy Goloviy <kykyagoloviy@gmail.com>
2025-07-03 19:06:00 +0300

    Add files via upload

---
+++ src/js/1-gallery.js
@@ -0,0 +1,20 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from './images';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const markup = images.map(({ preview, original, description }) =>
+  `<li class="gallery-item">
+    <a class="gallery-link" href="${original}">
+      <img class="gallery-image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`).join('');
+
+galleryContainer.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
