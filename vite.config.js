d17d3ba8f3bd0775c6732fcc1800c2a4cd5a709f
Andriy <kykyagoloviy@gmail.com>
2025-07-05 10:29:43 +0300

    Fix vite config: add htmlInject plugin

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,6 @@
-import { defineConfig } from 'vite';
-import glob from 'fast-glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import path from 'path';
+import htmlInject from 'vite-plugin-html-inject';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: '/goit-js-hw-09/',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      outDir: '../dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        input: glob.sync(path.resolve('src/*.html')),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) return 'vendor';
-          },
-          entryFileNames: chunk => chunk.name === 'commonHelpers' ? 'commonHelpers.js' : '[name].js',
-          assetFileNames: asset => asset.name?.endsWith('.html') ? '[name].[ext]' : 'assets/[name]-[hash][extname]'
-        }
-      }
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html'])
-    ]
-  };
-});
+export default {
+  base: '/goit-js-hw-09/',
+  plugins: [htmlInject()],
+};
